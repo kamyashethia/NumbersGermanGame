9add903b32780929e91ceb4d725e1423678627c2
kamya <kamya.shethia@girlswhocode.com>
2020-04-05 21:05:50 -0400

    background color and effects

--- index.js
+++ index.js
@@ -10,8 +10,20 @@ const NUMBERS = {
   6 : 'sechs',
   7 : 'seiben',
   8 : 'acht',
-  9 : 'neun'
+  9 : 'neun',
+  10 : 'zehn',
+  11 : 'elf',
+  12 : 'zwölf',
+  13 : 'dreizehn',
+  14 : 'vierzehn',
+  15 : 'funfzehn',
+  16 : 'sechszehn'
+
 }
+
+const correctAnswerBackground = "linear-gradient(to bottom left, #02B899 0%, #6EFEAF 100%)";
+const incorrectAnswerBackground = "linear-gradient(to bottom left, #660033 0%, #cc3300 100%)";
+const baseBackground = "linear-gradient(to bottom left, #3F356E 0%, #C7C1E3 100%)";
 
 window.onload = function() {
   var number = getRandomNumber();
@@ -30,30 +42,42 @@ window.onload = function() {
       console.log(userAnswer);
       console.log(actualAnswer);
       if (userAnswer == actualAnswer) {
-        console.log( 'tis correct');
-        number = getRandomNumber();
-        document.getElementById("number").innerHTML = number;
-        document.getElementById("answer").value = "";
-        console.log(document.getElementById("backgroundWrapper").style.backgroundColor);
-        document.getElementById("backgroundWrapper").style.background = "linear-gradient(to bottom left, #02B899 0%, #6EFEAF 100%)";
+          number = invokeCorrectAnswerResponse();
       } else {
-        console.log('tis wrong');
-        document.getElementById("answer").value = "";
-        document.getElementById("backgroundWrapper").style.background = "linear-gradient(to bottom left, #660033 0%, #cc3300 100%)";
-
-        //document.getElementById("number").innerHTML = getRandomNumber();
+        invokeIncorrectAnswerResponse();
       }
-
-
-      //show result
-
-      //display new numbers
 
     }
   });
 
 }
 
+function invokeCorrectAnswerResponse() {
+  console.log( 'tis correct');
+  var number2 = getRandomNumber();
+
+  document.getElementById("answer").value = "";
+  document.getElementById("backgroundWrapper").style.background = correctAnswerBackground;
+
+  setTimeout(function(){
+      document.getElementById("number").innerHTML = number2;
+      document.getElementById("backgroundWrapper").style.background = baseBackground;
+   }, 1000);
+
+  return number2;
+}
+
+function invokeIncorrectAnswerResponse() {
+  console.log('tis wrong');
+  document.getElementById("answer").value = "";
+  document.getElementById("backgroundWrapper").style.background = incorrectAnswerBackground;
+
+  setTimeout(function(){
+      document.getElementById("backgroundWrapper").style.background = baseBackground;
+   }, 1000);
+
+
+}
 
 function getRandomNumber() {
   return Math.floor(Math.random() * 10);
